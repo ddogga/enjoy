279bc763c779695ed5fb8f358058833062eeaa60
ddogga <hayeonlee60327@gmail.com>
2022-06-04 23:22:58 +0900

    Update Main

--- enjoy/src/main/java/com/lets/controller/MainController.java
+++ enjoy/src/main/java/com/lets/controller/MainController.java
@@ -1,5 +1,29 @@
 package com.lets.controller;
 
-public class MainController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.lets.service.MainService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Controller
+public class MainController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
+	
+	
+	@Autowired
+	private MainService mainservice;
+
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public ModelAndView main(ModelAndView mv) {
+		
+		
+		return mv;
+	}
 }
