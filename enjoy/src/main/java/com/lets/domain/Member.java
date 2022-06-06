37f97ae84940214d92c347430295b68b2efc1ea3
ddogga <hayeonlee60327@gmail.com>
2022-06-06 22:36:13 +0900

    Member update

---
+++ enjoy/src/main/java/com/lets/domain/Member.java
@@ -0,0 +1,5 @@
+package com.lets.domain;
+
+public class Member {
+
+}
--- enjoy/src/main/java/com/lets/service/MemberService.java
+++ enjoy/src/main/java/com/lets/service/MemberService.java
@@ -1,5 +1,22 @@
 package com.lets.service;
 
-public class MemberService {
+import com.lets.domain.Member;
 
+public interface MemberService {
+
+	public int insert(Member m) throws Exception;
+	
+	public int isId(String id);
+	
+	public Member checkId(String id);
+	
+	public Member member_info(String id);
+	
+	public void delete(String id);
+	
+	public int update(Member m);
+	
+	public void memberAuth(String memberEmail,String key,String id) throws Exception;
+	
+	public int newPW(Member m) throws Exception;
 }
